b18f89701b219872cd0fe3af3c3c804b6f7810d7
Sahil Babbar <sahilbabbar12.9@gmail.com>
2022-10-27 00:28:03 +0530

    Selection Sort

---
+++ SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class SelectionSort{
+    private static Scanner sc=new Scanner(System.in);
+    public static void main(String args[]){
+        int n;
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
